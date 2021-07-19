2cc557de4a33785ab220d6f04dc84da6674c1a17
fklotzb <f.klotzb@outlook.de>
2021-07-19 11:08:08 +0200

    add: ehum, serial printout; del: old files

--- drizzel.ino
+++ drizzel.ino
@@ -1,19 +1,12 @@
-#include <Scheduler.h>
-#include <Arduino_APDS9960.h>
-#include <Arduino_HTS221.h>
-#include <ArduinoBLE.h>
-
 #define EHUM_SENSOR A0
-#define VALVE A3
-#define LED_R 22
-#define LED_G 23
-#define LED_B 24
-#define LED_P 25
 #define LED_S 13
 
 void setup() {
+  // set pin modes
   pinMode(EHUM_SENSOR, INPUT);
+  pinMode(LED_S, OUTPUT);
   
+  // open serial port and wait to connect
   Serial.begin(9600);
   while (!Serial) {
     digitalWrite(LED_S, HIGH);
@@ -21,51 +14,20 @@ void setup() {
     digitalWrite(LED_S, LOW);
     delay(100);
   }
-  while (!APDS.begin()) {
-    Serial.println("ERROR: failed initializing APDS9960 sensor");
-    digitalWrite(LED_R, HIGH);
-    delay(10000);
-  }
-  while (!HTS.begin()) {
-    Serial.println("ERROR: failed initializing HTS221 sensor");
-    digitalWrite(LED_R, HIGH);
-    delay(10000);
-  }
-  digitalWrite(LED_R, LOW)
+  digitalWrite(LED_S, LOW);
 }
 
-
 void loop() {
+  // reset data
   ehum = 0;
-  abright = 0;
-  ahum = 0.0;
-  atemp = 0.0;
-  
+  // collect data
   for (byte i=0; i<100; i++) {
-    int r, g, b, a;
-    while (! APDS.colorAvailable()) {
-      delay(5);
-    }
-    ehum = ehum + analogRead(SENSOR);
-    APDS.readColor(r, g, b, a);
-    abright = abright + a;
-    ahum = ahum + HTS.readHumidity();
-    atemp = atemp + HTS.readTemperature();
+    ehum = ehum + analogRead(EHUM_SENSOR);
   }
-
+  // build avg
   ehum = ehum / 100;
-  abright = abright / 100;
-  ahum = ahum / 100;
-  atemp = atemp / 100;
-  
+  // print data
   Serial.print("EHUM= ");
-  Serial.print(ehum);
-  Serial.print("  ABRIGHT= ");
-  Serial.print(abright);
-  Serial.print("  AHUM= ");
-  Serial.print(ahum);
-  Serial.print("  ATEMP= ");
-  Serial.println(atemp);
-
+  Serial.println(ehum);
   delay(100);
 }
